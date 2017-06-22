31c34e9d1f372f8a0bca1056ded118647d1a0411
Ansel Santosa <anstosa@gmail.com>
2017-06-22 09:02:00 -0700

    Split library into separate script
    * Allows library to be updated by git independently of presentations
    * Makes presentation files more clean

--- scripted-presenter.sh
+++ scripted-presenter.sh
@@ -91,21 +91,3 @@ function focus_last() {
 function terminate() {
     tmux kill-session -t "${session}"
 }
-
-prompt "This is the first step, no need to wait here"
-run echo "Hello World"
-
-wait_and_prompt "First command done, moving on to the second one"
-run echo "Example command number two"
-
-wait_and_prompt "Let's start a ping down below..."
-run_below "-l 10" "ping localhost"
-
-wait_and_prompt "Now open a ping in a new window, but don't show that window"
-run_background "win-name" "ping localhost"
-
-wait_and_prompt "Finally, run a ping next to this"
-run_beside "-p 30" "ping localhost"
-
-wait_and_prompt "Pressing enter one more time will kill the whole demo"
-terminate
